772067ba292f07b9c7f9675415ad2def5d45badc
JoelAlexanderTrinidad <joelalexandertrinidad@gmail.com>
2023-01-12 20:35:32 -0300

    remove an unnecessary js

--- js/stats.js
+++ js/stats.js
@@ -1,146 +1,108 @@
-const $table = document.getElementById('main-table')
+const { createApp } = Vue
 
-fetch('https://mindhub-xj03.onrender.com/api/amazing')
-    .then(reponse => reponse.json())
-    .then(data => {
-        const eventos = data.events
-        const currentDate = data.currentDate
-        const eventosFuturos = eventos.filter(evento => evento.estimate)
-        const eventosPasados = eventos.filter(evento => evento.assistance)
-        const categorias = eventos.map(evento => evento.category)
-        const categoriasSinRepetir =  Array.from(new Set(categorias))
+createApp({
+    data(){
+        return {
+            eventos: [],
+            mayorAsistencia: [],
+            menorAsistencia: [],
+            mayorCapacidad: [],
+            estadFuturas: [],
+            estadPasadas: [],
+        }
+    },
+    created(){
+        fetch('https://mindhub-xj03.onrender.com/api/amazing')
+            .then(response => response.json())
+            .then(data => {
+                let fechaActual = data.currentDate
+                this.eventos = data.events
+                const eventosPasados = data.events.filter(evento => evento.assistance)
+                const eventosFuturos = data.events.filter(evento => evento.estimate)
+                this.mayorAsistencia = this.eventosMayorAsistencia(eventosPasados)
+                this.menorAsistencia = this.eventosMenorAsistencia(eventosPasados)
+                this.mayorCapacidad = this.eventoMayorCapacidad(this.eventos)
 
-        const gananciasDeEventosFuturos =  gananciasEventos(eventosFuturos, currentDate) 
+                this.categorias = [... new Set( this.eventos.map(evento => evento.category))]
 
-        const gananciasDeEventosPasados =  gananciasEventos(eventosPasados, currentDate)
+                console.log()
 
-        const porcentajeFuturo = porcentajeDeGanancia(eventosFuturos)
-        const porcentajePasado = porcentajeDeGanancia(eventosPasados)
-       
-        let mayor = ((eventosFuturos[0].estimate / eventosFuturos[0].capacity) * 100)
+                const gananciasPasadas = this.ganancias(eventosPasados, fechaActual)
+                const gananciasFuturas = this.ganancias(eventosFuturos, fechaActual)
 
-        for (let i = 0; i < eventosFuturos.length; i++) {
-            if(((eventosFuturos[i].estimate / eventosFuturos[i].capacity) * 100) > mayor){
-                mayor = ((eventosFuturos[i].estimate / eventosFuturos[i].capacity) * 100) 
+                const porcentajeFuturo = this.porcentajes(eventosFuturos)
+                const porcentajePasado = this.porcentajes(eventosPasados)
+
+                this.estadFuturas = this.categorias.map((item, i) => ({
+                    categoria: item,
+                    ganancia: gananciasFuturas[i],
+                    porcentaje: porcentajeFuturo[i]
+                }))
+
+                this.estadPasadas = this.categorias.map((item, i) => ({
+                    categoria: item,
+                    ganancia: gananciasPasadas[i],
+                    porcentaje: porcentajePasado[i]
+                }))
+              
+
+            })
+            .catch(error => console.log(error))
+    },
+    methods: {
+        eventosMayorAsistencia: function(eventosPasados){
+            let asistencia = (eventosPasados[0].assistance /  eventosPasados[0].capacity) * 100
+            let eventoMayorAsistencia = eventosPasados[0]
+
+            eventosPasados.forEach(evento => {
+                if((evento.assistance / evento.capacity) * 100 > asistencia){
+                    asistencia = (evento.assistance / evento.capacity) * 100
+                    eventoMayorAsistencia = evento
+                }
+            });
+            return eventoMayorAsistencia
+        },
+        eventosMenorAsistencia: function(eventosPasados){
+            let asistencia = (eventosPasados[0].assistance /  eventosPasados[0].capacity) * 100
+            let eventoMenorAsistencia = eventosPasados[0]
+
+            eventosPasados.forEach(evento => {
+                if((evento.assistance / evento.capacity) * 100 < asistencia){
+                    asistencia = (evento.assistance / evento.capacity) * 100
+                    eventoMenorAsistencia = evento
+                }
+            });
+            return eventoMenorAsistencia
+        },
+        eventoMayorCapacidad: function(eventos){
+            let mayorCapacidad = eventos[0].capacity
+            let eventoMayor = eventos[0]
+            eventos.forEach(evento => {
+                if(evento.capacity > mayorCapacidad){
+                    mayorCapacidad = evento.capacity
+                    eventoMayor = evento
+                }
+
+            });
+            return eventoMayor
+        },
+        ganancias: function(eventos, fechaActual){
+            if(eventos[0].estimate){
+                const ganancias = eventos.filter(evento => evento.date > fechaActual).map(evento => evento.estimate * evento.price)
+                return ganancias
+            }else{
+                const ganancias = eventos.filter(evento => evento.date < fechaActual).map(evento => evento.assistance * evento.price)
+                return ganancias
+            }
+        },
+        porcentajes: function(eventos){
+            if(eventos[0].estimate){
+                const porcentaje = eventos.map(evento => Math.round( (((evento.estimate / evento.capacity) * 100) * 100)) / 100 );
+                return porcentaje
+            }else{
+                const porcentaje = eventos.map(evento => Math.round( (((evento.assistance / evento.capacity) * 100) * 100)) / 100 );
+                return porcentaje
             }
         }
-
-        let menor = ((eventosPasados[0].assistance / eventosPasados[0].capacity) * 100)
-
-        for (let i = 0; i < eventosPasados.length; i++) {
-            if(((eventosPasados[i].assistance / eventosPasados[i].capacity) * 100) < menor){
-                menor = ((eventosPasados[i].assistance / eventosPasados[i].capacity) * 100)
-            }
-        }
-        
-        let mayorPorcentaje = eventos.find(evento => (evento.estimate / evento.capacity) * 100 === mayor || (evento.assistance / evento.capacity) * 100 === mayor)
-        let menorPorcentaje = eventos.find(evento => (evento.estimate / evento.capacity) * 100 === menor || (evento.assistance / evento.capacity) * 100 === menor)
-
-        let mayorCapacidad = eventos[0].capacity
-
-        for (let i = 0; i < eventos.length; i++) {
-            if(eventos[i].capacity > mayorCapacidad){
-                mayorCapacidad = eventos[i].capacity
-            }
-        }
-        let eventoMayorCapacidad = eventos.find(evento => evento.capacity === mayorCapacidad)
-
-        renderizar(crearTabla(categoriasSinRepetir, gananciasDeEventosFuturos, porcentajeFuturo, gananciasDeEventosPasados, porcentajePasado, mayorPorcentaje, menorPorcentaje, eventoMayorCapacidad), 'main-table') // recibe dos argumentos (template y contenedor)
-    })
-    .catch(error => console.log(error))
-
-
-function porcentajeDeGanancia(eventos){
-    if(eventos[0].estimate){
-        const porcentaje = eventos.map(evento => Math.round( (((evento.estimate / evento.capacity) * 100) * 100)) / 100 );
-        return porcentaje
-    }else{
-        const porcentaje = eventos.map(evento => Math.round( (((evento.assistance / evento.capacity) * 100) * 100)) / 100 );
-        return porcentaje
     }
-}
-
-function gananciasEventos(eventos, fechaActual){
-    if(eventos[0].estimate){
-        const ganancias = eventos.filter(evento => evento.date > fechaActual).map(evento => evento.estimate * evento.price)
-        return ganancias
-    }else{
-        const ganancias = eventos.filter(evento => evento.date < fechaActual).map(evento => evento.assistance * evento.price)
-        return ganancias
-    }
-}
-
-function eventosTabla(categorias, ganancias, porcentaje){
-    let template = ''
-
-    for (let i = 0; i < categorias.length; i++) {
-        template += 
-            `
-                <tr>
-                    <td class="th">${categorias[i]}</td>
-                    <td class="th">${ganancias[i]}</td>
-                    <td class="th">${porcentaje[i]}</td>
-                </tr>
-            
-            `
-    }
-    return template
-}
-
-function crearTabla(categorias, gananciasDeEventosFuturos, porcentajeFuturo, gananciasPasadas, porcentajePasado,  porcentajeMasAlto, porcentajeMasBajo, mayorCapacidad){
-    let template = ''
-    template = `
-    <table>
-        <thead>
-            <tr>
-                <th class="titulo-tabla" colspan="3">Events statistics</th>
-            </tr>
-            <tr>
-                <th class="th">Events with highest percentage of attendance</th>
-                <th class="th">Events with the lowest percentage of attendance</th>
-                <th class="th">Event with larger capacity</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td class="th">${porcentajeMasAlto.name}</td>
-                <td class="th">${porcentajeMasBajo.name}</td>
-                <td class="th">${mayorCapacidad.name}</td>
-            </tr>
-        </tbody>
-
-        <thead>
-            <tr>
-                <th class="titulo-tabla" colspan="3">Upcoming events statistics by category</th>
-            </tr>
-            <tr>
-                <th class="th">Categories</th>
-                <th class="th">Revenues</th>
-                <th class="th">Percentage of attendance</th>
-            </tr>
-        </thead>
-        <tbody>
-            ${eventosTabla(categorias, gananciasDeEventosFuturos, porcentajeFuturo)}
-        </tbody>
-
-        <thead>
-            <tr>
-                <th class="titulo-tabla" colspan="3">Past events statistics by category</th>
-            </tr>
-            <tr>
-                <th class="th">Categories</th>
-                <th class="th">Revenues</th>
-                <th class="th">Percentage of attendance</th>
-            </tr>
-        </thead>
-        <tbody>
-            ${eventosTabla(categorias, gananciasPasadas, porcentajePasado)}
-        </tbody>
-    </table>
-    `
-    return template
-}
-
-function renderizar(template, contenedor){
-    document.getElementById(contenedor).innerHTML = template
-}
+}).mount("#app")
